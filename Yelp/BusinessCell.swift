f9a6d9de557bd9b949813dfc5bbb434bcd49a486
Chris Lin <pwnzerness@gmail.com>
2018-01-28 17:05:05 -0500

    added labels and image views to cells

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,38 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Chris lin on 1/28/18.
+//  Copyright © 2018 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingsImageView: UIImageView!
+    @IBOutlet weak var reviewCountLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    var business: Business!{
+        didSet{
+            nameLabel.text = business.name
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
